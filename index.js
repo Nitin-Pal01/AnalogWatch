6fe27035b0dff18302fad4cd27642ec95cd5c8a3
Nitin Pal <155223372+Nitin-Pal01@users.noreply.github.com>
2023-12-31 17:42:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,58 +1,22 @@
-var dialLines = document.getElementsByClassName('diallines');
-var clockEl = document.getElementsByClassName('clock')[0];
+ let hr = document.getflemntByID('hour');
+let min = document.getflemntByID('minute');
+let sec = document.getflemntByID('second');
 
-for (var i = 1; i < 60; i++) {
-  clockEl.innerHTML += "<div class='diallines'></div>";
-  dialLines[i].style.transform = "rotate(" + 6 * i + "deg)";
+function displayTime(){
+    let date = new Date();
+
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSecond();
+
+    let hRtation = 30*hh + mm/2;
+    let mRotation = 6*mm;
+    let sRotation = 6*ss;
+
+    hr.style.transform = 'rotate(${hRotation}deg)';
+    min.style.transform = 'rotate(${mRotation}deg)';
+    sec.style.transform = 'rotate(${sRotation}deg)';
+
+
 }
-
-function clock() {
-  var weekday = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday"
-      ],
-      d = new Date(),
-      h = d.getHours(),
-      m = d.getMinutes(),
-      s = d.getSeconds(),
-      date = d.getDate(),
-      month = d.getMonth() + 1,
-      year = d.getFullYear(),
-           
-      hDeg = h * 30 + m * (360/720),
-      mDeg = m * 6 + s * (360/3600),
-      sDeg = s * 6,
-	  s = d.getSeconds(),
-date = d.getDate(),
-month = d.getMonth() + 1,
-year = d.getFullYear(),
-	 
-hDeg = h * 30 + m * (360/720),
-mDeg = m * 6 + s * (360/3600),
-sDeg = s * 6,
-
-hEl = document.querySelector('.hour-hand'),
-mEl = document.querySelector('.minute-hand'),
-sEl = document.querySelector('.second-hand'),
-dateEl = document.querySelector('.date'),
-dayEl = document.querySelector('.day');
-
-var day = weekday[d.getDay()];
-
-if(month < 9) {
-month = "0" + month;
-}
-
-hEl.style.transform = "rotate("+hDeg+"deg)";
-mEl.style.transform = "rotate("+mDeg+"deg)";
-sEl.style.transform = "rotate("+sDeg+"deg)";
-dateEl.innerHTML = date+"/"+month+"/"+year;
-dayEl.innerHTML = day;
-}
-
-setInterval("clock()", 100);
+setInterval(displayTime,1000);
